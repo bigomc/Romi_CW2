63366256894d3cc5c08a85add8db77a364a52626
MarioEstebanOchoa <45211069+MarioEstebanOchoa@users.noreply.github.com>
2019-03-28 14:30:04 +0000

    I create new solution files to work with my VisualStudio, dont worry about it, just ignore those files

---
+++ Romi/Release/Romi.cpp.d
@@ -0,0 +1,44 @@
+C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\Romi.cpp.o: \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\Romi.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\leonardo/pins_arduino.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\pins.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\utils.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\motors.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\pid.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\interrupts.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\kinematics.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\line_sensors.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\irproximity.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\mapping.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\EEPROM\src/EEPROM.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\RF_Interface.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\RFID.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/DigitalIO.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/DigitalPin.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/boards/GpioPinMap.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/boards/LeonardoGpioPinMap.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/I2cConstants.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/PinIO.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/SoftI2cMaster.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\DigitalIO\src/SoftSPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\imu.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\LSM6/LSM6.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\magnetometer.h \
+ C:\Users\Esteban\Documents\Arduino\libraries\LIS3MDL/LIS3MDL.h \
+ C:\Users\Esteban\AppData\Local\Temp\VMBuilds\Romi\leonardo\Release\Pushbutton.h
