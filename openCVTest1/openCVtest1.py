6ceda7676a74404ffa83d87efbd2f361c080c514
YusseffRuiz <zeiverking@gmail.com>
2019-04-02 21:50:23 +0100

    changes in code for the weigth

--- openCVTest1/openCVtest1.py
+++ openCVTest1/openCVtest1.py
@@ -8,14 +8,40 @@ def subdivide(image):
     rows, cols, chann = image.shape
     x = 0
     y = 0
+    weight = 0
     while(y<rows-gridsize):
         x = 0
         while(x<cols-gridsize):
             k = x*y+x
             cv2.rectangle(outputimage, (x,y), (x+gridsize,y+gridsize), (0, 0, 255), 1)
+            weight += weightImage(outputimage, x , y, gridsize)
             x += gridsize
         y += gridsize
-    return outputimage
+    return [outputimage, weight]
+
+def weightImage(image, x, y, gridsize):
+    auximage = image[x:x + gridsize, y:y + gridsize]
+    # grab the image dimensions
+    h = auximage.shape[0]
+    w = auximage.shape[1]
+    weigthsum = 0
+
+    # loop over the image, pixel by pixel
+    for y in range(0, h):
+        for x in range(0, w):
+            # look at the values of each pixel
+            px = auximage[y,x]
+            bluepx = px[0]
+            greenpx = px[1]
+            redpx = px[2]
+            if(bluepx != 255 and greenpx != 255):
+                weigthsum += 0
+            else:
+                weigthsum += 1
+    if weigthsum > 0:
+        return 0
+    else:
+        return 1
 
 
 nameOfImg = 'Photos/WellDoneMap2.jpg'
@@ -35,10 +61,11 @@ for line in lines:
     cv2.line(blankImg, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
 
-subdivide(blankImg)
+[dividedImage, weightSum] = subdivide(blankImg)
 
 cv2.imshow("Edges", img)
-cv2.imshow("Subdivided", blankImg)
+cv2.imshow("Subdivided", dividedImage)
+print(weightSum)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
