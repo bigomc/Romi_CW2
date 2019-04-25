e45e436e11e07b289022bffd8696a9ebfc6498a3
MarioEstebanOchoa <45211069+MarioEstebanOchoa@users.noreply.github.com>
2019-04-25 21:05:54 +0100

    show internal map

--- Matlab/exploration.m
+++ Matlab/exploration.m
@@ -21,69 +21,24 @@ MAP_R=2*(ones(MAX_X,MAX_Y));
 j=0;
 x_val = 1;
 y_val = 1;
-axis([1 MAX_X 1 MAX_Y])
+axis([1 MAX_X+1 1 MAX_Y+1])
 grid on;
 grid minor;
 hold on;
 n=0;%Number of Obstacles
 
 %%
-% BEGIN Interactive Obstacle, Target, Start Location selection
-pause(1);
-h=msgbox('Please Select the Target using the Left Mouse button');
-uiwait(h,5);
-if ishandle(h) == 1
-    delete(h);
-end
-xlabel('Please Select the Target using the Left Mouse button','Color','black');
-but=0;
-while (but ~= 1) %Repeat until the Left button is not clicked
-    [xval,yval,but]=ginput(1);
-end
-xval=floor(xval);
-yval=floor(yval);
-xTarget=xval;%X Coordinate of the Target
-yTarget=yval;%Y Coordinate of the Target
 
-MAP(xval,yval)=0;%Initialize MAP with location of the target
-plot(xval+.5,yval+.5,'gd');
-text(xval+1,yval+.5,'Target')
-
-pause(2);
-h=msgbox('Select Obstacles using the Left Mouse button,to select the last obstacle use the Right button');
-  xlabel('Select Obstacles using the Left Mouse button,to select the last obstacle use the Right button','Color','blue');
-uiwait(h,10);
-if ishandle(h) == 1
-    delete(h);
-end
-while but == 1
-    [xval,yval,but] = ginput(1);
-    xval=floor(xval);
-    yval=floor(yval);
-    MAP(xval,yval)=-1;%Put on the closed list as well
-    plot(xval+.5,yval+.5,'ro');
- end%End of While loop
+[m,n] = size(MAP);
  
-%  pause(1);
-% 
-% h=msgbox('Please Select the Vehicle initial position using the Left Mouse button');
-% uiwait(h,5);
-% if ishandle(h) == 1
-%     delete(h);
-% end
-% xlabel('Please Select the Vehicle initial position ','Color','black');
-% but=0;
-% while (but ~= 1) %Repeat until the Left button is not clicked
-%     [xval,yval,but]=ginput(1);
-%     xval=floor(xval);
-%     yval=floor(yval);
-% end
-% xStart=xval;%Starting Position
-% yStart=yval;%Starting Position
-% MAP(xval,yval)=1;
-%  plot(xval+.5,yval+.5,'bo');
-%End of obstacle-Target pickup
-
+ for i=1:1:m
+     for j=1:1:n
+         if MAP(i,j)==-1
+             plot(i+0.5,j+0.5,'ro');
+         end
+     end
+ end
+ 
 x_init = 2;
 y_init = 2;
 
@@ -91,21 +46,37 @@ p = plot(x_init+0.5,y_init+0.5,'bo');
 
 pause(2)
 
-exploration_path=[2 0;2 1;2 2;2 3;2 4;2 5];
 %%
 heading = 1;
 x = 2;
 y = 2;
-while true
+while x<24
     sensors = check_sensors(x,y,heading,MAP);
     MAP_R = mapping(x,y,heading,sensors,MAP_R);
     [x,y,heading] = move(x,y,heading,sensors);
-    pause(.25);
+    %pause(.25);
     set(p,'XData',x+0.5,'YData',y+0.5);
     xlabel(heading,'Color','blue');
     drawnow ;
 end
 
+figure(2)
+[m,n] = size(MAP);
+ 
+ for i=1:1:m
+     for j=1:1:n
+         if MAP_R(i,j)==0
+             plot(i+0.5,j+0.5,'bo');
+         end
+         if MAP_R(i,j)==1
+             plot(i+0.5,j+0.5,'b*');
+         end
+         if MAP_R(i,j)==2
+             plot(i+0.5,j+0.5,'r*');
+         end
+         hold on;
+     end
+ end
 
 % switch heading 
 %    case 1   %UP
