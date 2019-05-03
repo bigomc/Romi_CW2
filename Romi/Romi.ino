ee27c8693bec588cbf342cd6210746b70ca462c9
Rodrigo Moreno <bigomc@gmail.com>
2019-05-03 15:55:56 +0100

    Goal reached if got stucked for a while

--- Romi/Romi.ino
+++ Romi/Romi.ino
@@ -34,8 +34,8 @@
 #define BAUD_RATE 115200
 #define SAMPLING_TICK_PERIOD    5
 #define MAX_VELOCITY    3
-#define TIME_LIMIT  1800000
-#define LINE_CONFIDENCE 70
+#define TIME_LIMIT  180000
+#define LINE_CONFIDENCE 50
 #define VMAX    3
 //#define USE_MAGNETOMETER    1     //To use magnetometer uncomment this line
 #define USE_OBSTACLE_AVOIDANCE  1
@@ -68,8 +68,8 @@ Motor         RightMotor(MOTOR_PWM_R, MOTOR_DIR_R);
 //These work for our Romi - We strongly suggest you perform your own tuning
 PID           LeftSpeedControl( 10, 0.1, 1 );
 PID           RightSpeedControl( 10, 0.1, 1 );
-PID           HeadingControl( 7, 0, 7 );
-PID           TurningControl( 1, 0, 0.6 );
+PID           HeadingControl( 9, 0, 9 );
+PID           TurningControl( 1, 0, 0.7 );
 
 Mapper        Map; //Class for representing the map
 
@@ -88,7 +88,9 @@ Pushbutton    ButtonB( BUTTON_B, DEFAULT_STATE_HIGH);
  float y_error;
  float orientation_error;
  float position_error;
- const float Ks = PI/8;
+ float last_distance_error;
+ int stucked_counter = 0;
+ const float Ks = PI/4;
 
  // Planning Variables
  volatile bool goal_reached = false;
@@ -205,7 +207,7 @@ void setup()
   // initialised, which will cause a big intergral term.
   // If you don't do this, you'll see the Romi accelerate away
   // very fast!
-    HeadingControl.setMax(2 * VMAX);
+    HeadingControl.setMax(VMAX);
     TurningControl.setMax(VMAX);
     LeftSpeedControl.setMax(2 * VMAX);
     RightSpeedControl.setMax(2 * VMAX);
@@ -329,21 +331,24 @@ void ControlSpeed() {
 void ControlPosition() {
     float sat;
     float offset = 0;
-    float turning;
-    float ahead;
     float x_error = goal.x - Pose.getX();
     float y_error = goal.y - Pose.getY();
     Point_t direction;
+    float distance_error;
+    float turning;
+    float ahead;
 
     direction = obstacleAvoidanceSensors (goal.x, goal.y);
-    mag = direction.x;
-    ang = direction.y - Pose.getThetaRadians();
 
 #ifdef USE_OBSTACLE_AVOIDANCE
+    last_distance_error = distance_error;
     position_error = direction.x;
+    distance_error = sqrt(x_error*x_error + y_error*y_error);
     orientation_error = direction.y - Pose.getThetaRadians();
 #else
+    last_distance_error = distance_error;
     position_error = sqrt(x_error*x_error + y_error*y_error);
+    distance_error = position_error;
     if(position_error > 1) {
         position_error = 1;
     }
@@ -357,7 +362,11 @@ void ControlPosition() {
         orientation_error -= (2 * PI);
     }
 
-    if(sqrt(x_error*x_error + y_error*y_error) > 5) {
+    if(last_distance_error - distance_error < 10) {
+        stucked_counter++;
+    }
+
+    if((distance_error > 30) && (stucked_counter < 200)) {
         sat = min(Ks, max(-Ks, orientation_error));
 
         turning = TurningControl.update(orientation_error, 0);
@@ -368,6 +377,7 @@ void ControlPosition() {
         right_speed_demand = ahead + turning;
     } else {
         goal_reached = true;
+        stucked_counter = 0;
 
         left_speed_demand = 0;
         right_speed_demand = 0;
@@ -441,8 +451,6 @@ void MappingTask() {
     if(distance < max_confidence) {
         coordinate = getObstacleCoordinates(distance, sensors_offset[SENSOR_LEFT], true);
         Map.updateMapFeature(Map.OBSTACLE, coordinate.y, coordinate.x );
-        coordinate = getObstacleCoordinates(distance - 72, sensors_offset[SENSOR_LEFT], true);
-        Map.updateMapFeature(Map.BORDER, coordinate.y, coordinate.x );
     }
 
     // Sensor front
@@ -454,8 +462,6 @@ void MappingTask() {
     if(distance < 2*max_confidence) {
         coordinate = getObstacleCoordinates(distance, sensors_offset[SENSOR_FRONT], true);
         Map.updateMapFeature(Map.OBSTACLE, coordinate.y, coordinate.x );
-        coordinate = getObstacleCoordinates(distance - 72, sensors_offset[SENSOR_FRONT], true);
-        Map.updateMapFeature(Map.BORDER, coordinate.y, coordinate.x );
     }
 
     // Sensor front 2
@@ -467,8 +473,6 @@ void MappingTask() {
     if(distance < 2*max_confidence) {
         coordinate = getObstacleCoordinates(distance, sensors_offset[SENSOR_FRONT2], true);
         Map.updateMapFeature(Map.OBSTACLE, coordinate.y, coordinate.x );
-        coordinate = getObstacleCoordinates(distance - 72, sensors_offset[SENSOR_FRONT2], true);
-        Map.updateMapFeature(Map.BORDER, coordinate.y, coordinate.x );
     }
 
     // Right sensor
@@ -480,8 +484,6 @@ void MappingTask() {
     if(distance < max_confidence) {
         coordinate = getObstacleCoordinates(distance, sensors_offset[SENSOR_RIGHT], true);
         Map.updateMapFeature(Map.OBSTACLE, coordinate.y, coordinate.x );
-        coordinate = getObstacleCoordinates(distance - 72, sensors_offset[SENSOR_RIGHT], true);
-        Map.updateMapFeature(Map.BORDER, coordinate.y, coordinate.x );
     }
 
 
@@ -510,7 +512,7 @@ void MappingTask() {
     // Basic uncalibrated check for a line.
     // Students can do better than this after CW1 ;)
     // Condition will depend on calibration method, the one below worked for my Romi using static calibration
-    if( (LineCentre.readCalibrated() + LineLeft.readCalibrated() + LineRight.readCalibrated()) > LINE_CONFIDENCE  ) {
+    if( (LineLeft.readCalibrated() + LineRight.readCalibrated()) > LINE_CONFIDENCE  ) {
         Map.updateMapFeature(Map.LINE, Pose.getY(), Pose.getX() );
     }
 
@@ -536,8 +538,8 @@ Point_t getObstacleCoordinates(float distance, float orientation_offset, bool ma
 
 Point_t obstacleAvoidanceSensors(float x_goal, float y_goal){
     Point_t result;
-    const float Kg = 0.001; //This can be updated to achieve good obstacle avoidance response
-    const float Ko = 1000000; //This can be updated to achieve good obstacle avoidance response
+    const float Kg = 0.01; //This can be updated to achieve good obstacle avoidance response
+    const float Ko = 1000; //This can be updated to achieve good obstacle avoidance response
 
     //Initialise total resultant forces
     float Fx_total = 0;
